5e65e223f2c1088c2bf47a91ae5e6914b2ec7a8f
Matthew Haddock <777mth@gmail.com>
2018-01-06 18:18:35 -0500

    clean up

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,12 +1,8 @@
-// $(function() { // begin ready() on document load
-  
 var topics = ["mario brothers", "legend of zelda", "donkey kong", "dragon's lair", "starfox", "gauntlet", "ghosts n' goblins", "castlevania"];
   
 var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=k3AP0b4ishUDqIHJqBK4L4TrpDRDpjw8&limit=10&q='";
-console.log("start of program, topics[] count = "+topics.length);
 
 function createButton(value) {
-  console.log("execute createButton()");
   var button = $("<button>");
   button.addClass("topicButton");
   button.attr("value", value);
@@ -16,7 +12,6 @@ function createButton(value) {
 }
 
 function displayTopics() {
-  console.log("execute displayTopics()");
   for (i = 0 ; i < topics.length ; i++) {
     createButton(topics[i]);
   };
@@ -24,25 +19,19 @@ function displayTopics() {
 
 displayTopics();
 
-// creates new button from query field
 $("#submit").on("click", function() {
   event.preventDefault();
-  console.log("new query submit clicked");
   var newQuery = $.trim($("#field").val());
   if (newQuery == "" || topics.includes(newQuery)) {
     $("#field").val("");
-    console.log("newQuery field invalid");
   } else {
     topics.push(newQuery);
-    console.log("newQuery topics count = ", topics.length);
     createButton(newQuery);
     $("#field").val("");    
   }
-  console.log("past 'if'");
 });
 
 function addImages() {
-  console.log("execute addImages()");
   $("#results").empty();
   $.ajax({
     url: queryURL+$(this).val(),
@@ -54,7 +43,6 @@ function addImages() {
     };
     
     $("img").on("click", function() {
-      console.log("image click");
       if ($(this).attr("still") === "true") {
         $(this).attr("src", response.data[$(this).attr("value")].images.fixed_width.url);
         $(this).attr("still", "false");
@@ -65,6 +53,3 @@ function addImages() {
     });
   })
 };
-  
-
-// }); // end ready() on document load
